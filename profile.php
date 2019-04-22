dfbd724e6dafc57974cfddba2ce1ae9b67e242ef
Tamilvananb <43129537+Tamilvananb@users.noreply.github.com>
2019-04-22 16:00:06 +0530

    Update profile.php

--- profile.php
+++ profile.php
@@ -6,10 +6,10 @@ if (!isset($_SESSION['loggedin'])) {
 	header('Location: index.html');
 	exit();
 }
-$DATABASE_HOST = 'localhost';
-$DATABASE_USER = 'root';
-$DATABASE_PASS = 'Zippyops@2018';
-$DATABASE_NAME = 'test';
+$DATABASE_HOST = 'wpdb.cdy9kerizbgn.ap-southeast-1.rds.amazonaws.com';
+$DATABASE_USER = 'tamizh';
+$DATABASE_PASS = 'mypassword';
+$DATABASE_NAME = 'wordpress_db';
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if (mysqli_connect_errno()) {
 	die ('Failed to connect to MySQL: ' . mysqli_connect_error());
