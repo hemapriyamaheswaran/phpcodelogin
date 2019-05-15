d55753ac7fa1d38ce3c953dc0e50a36dd1144cba
hemapriyamaheswaran <45891957+hemapriyamaheswaran@users.noreply.github.com>
2019-05-15 15:05:45 +0530

    Update profile.php

--- profile.php
+++ profile.php
@@ -7,9 +7,9 @@ if (!isset($_SESSION['loggedin'])) {
 	exit();
 }
 $DATABASE_HOST = 'hemapriyadb.mysql.database.azure.com';
-$DATABASE_USER = 'hema';
-$DATABASE_PASS = 'mypassword';
-$DATABASE_NAME = 'wordpress_db';
+$DATABASE_USER = 'hemapriya';
+$DATABASE_PASS = 'ZIPPYOPSzipp11';
+$DATABASE_NAME = 'exampledb';
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if (mysqli_connect_errno()) {
 	die ('Failed to connect to MySQL: ' . mysqli_connect_error());
